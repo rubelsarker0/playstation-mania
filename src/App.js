6b845a078d718e1c24e33942fb2b24be9d9bff49
rubelsarker0 <rubelsarker0@outlook.com>
2022-04-04 14:30:41 +0100

    added-routes-path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
+import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Header></Header>
-		</div>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/home" element={<Home />}></Route>
+				<Route path="/reviews" element={<Reviews />}></Route>
+				<Route path="/dashboard" element={<Dashboard />}></Route>
+				<Route path="/blogs" element={<Blogs />}></Route>
+				<Route path="/about" element={<About />}></Route>
+				<Route path="*" element={<NotFound />}></Route>
+			</Routes>
+			<Footer></Footer>
+		</>
 	);
 }
 
