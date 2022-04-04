a6d2aace0cc84b07033bb41f6a4c3cb6e7b9c58e
rubelsarker0 <rubelsarker0@outlook.com>
2022-04-04 14:45:40 +0100

    added-footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className="footer-bg-color text-white py-5">
+			<p className="text-white p-3 m-0 text-center">
+				Develop by
+				<a
+					className="ms-2 footer-text-color"
+					target="_blank"
+					rel="noreferrer"
+					href="https://www.facebook.com/rubelsarker8"
+				>
+					Rubel Sarker
+				</a>
+				<span className="ms-2">&copy; 2022 all rights reserved</span>
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
